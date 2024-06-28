1d68a8395b5057c323195ee7ed70ddd81a1fe434
ZhengWei112 <2021141460333@stu.scu.edu.cn>
2024-06-28 19:02:08 +0800

    搭建后端框架

--- src/main/java/com/example/mybatisplus/common/utls/SecurityUtils.java
+++ src/main/java/com/example/mybatisplus/common/utls/SecurityUtils.java
@@ -14,10 +14,12 @@ public class SecurityUtils {
      */
     public static UserInfoDTO getCurrentUserInfo() {
         UserInfoDTO userInfo = SessionUtils.getCurrentUserInfo();
-        //模拟登录
+        // 模拟登录 TODO: 因为现在还没有登录功能 以后要改
         if (userInfo == null) {
             userInfo = new UserInfoDTO();
+            userInfo.setId(1L);
             userInfo.setName("模拟用户");
+            userInfo.setUserType(1L);
         }
 
         return userInfo;
